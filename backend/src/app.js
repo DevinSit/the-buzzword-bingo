73e4fcf76dd271792e85296e7b84f31d6192d5f4
Devin Sit <devin@devinsit.com>
2020-01-17 14:16:02 -0500

    Adding comments to backend.

--- backend/src/app.js
+++ backend/src/app.js
@@ -9,6 +9,7 @@ const API_ROOT = "/api/v1";
 
 const app = express();
 
+// Enable security, CORS, compression, and body parsing
 app.use(cors());
 app.options("*", cors());
 app.use(express.json());
@@ -16,8 +17,10 @@ app.use(express.urlencoded({extended: false}));
 app.use(bodyParser.json());
 app.use(morgan(":remote-addr - :remote-user [:date[clf]] \":method :url HTTP/:http-version\" :status :res[content-length] :response-time ms"));  // eslint-disable-line
 
+// Setup controllers
 app.use(`${API_ROOT}/users`, usersController);
 
+// Error handling must be the last middleware
 app.use(errorHandler());
 
 module.exports = app;
